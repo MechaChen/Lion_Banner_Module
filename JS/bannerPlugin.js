6a37a91cb5c41e13e28b628dd199297d2dcd2130
Benson <tumlivein@gmail.com>
2019-08-26 11:12:10 +0800

    2. 函式加入連鎖功能

--- JS/bannerPlugin.js
+++ JS/bannerPlugin.js
@@ -1,7 +1,10 @@
 $.fn.banner = function() {
   $(this).css("boxShadow", "3px 3px 3px #a3a3a3");
+  return this;
 };
 
 $(function() {
-  $(".banner").banner();
+  $(".banner")
+    .banner()
+    .addClass("test_class");
 });
