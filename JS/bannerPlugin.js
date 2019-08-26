e7e13f5ede1d949421aa266a72a94bbbd4a5bb04
Benson <tumlivein@gmail.com>
2019-08-26 11:57:16 +0800

    6. 擴充 Module Prototype 的函式和函式測試

--- JS/bannerPlugin.js
+++ JS/bannerPlugin.js
@@ -1,4 +1,5 @@
 (function($) {
+  "use strict";
   var ModuleName = "banner";
 
   var Module = function(ele, options) {
@@ -8,10 +9,27 @@
   };
 
   var shadow = "3px 3px 3px #a3a3a3";
+  var skewAngle = "skew(10deg)";
+
+  Module.prototype.addShadow = function() {
+    // console.log("this is a prototype func!!");
+    this.$ele.css("boxShadow", shadow);
+  };
+  Module.prototype.skew = function() {
+    this.$ele.css("transform", skewAngle);
+  };
 
   $.fn[ModuleName] = function(methods, options) {
     return this.each(function(index, el) {
-      $(el).css("boxShadow", shadow);
+      // $(el).css("boxShadow", shadow);
+      var opts = $.extend(
+        {},
+        typeof methods == "object" && methods,
+        typeof options == "object" && options
+      );
+      var module = new Module(el, opts);
+      module.addShadow();
+      module.skew();
     });
   };
 })(jQuery);
