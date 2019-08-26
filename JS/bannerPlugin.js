029068aac0cf6fa5bc80ac6e2235db990f24ea62
Benson <tumlivein@gmail.com>
2019-08-26 17:44:35 +0800

    完成 closed, closing, opened, opening 四階段

--- JS/bannerPlugin.js
+++ JS/bannerPlugin.js
@@ -10,11 +10,6 @@
     this.options = options;
   };
 
-  var shadow = "3px 3px 3px #a3a3a3";
-  var closeHeight = "translateY(-300px)";
-  var openHeight = "translateY(0px)";
-  var transSecond = "2s";
-
   Module.DEFAULTS = {
     openAtStart: true,
     autoToggle: true,
@@ -35,15 +30,21 @@
     }
   };
 
+  var shadow = "3px 3px 3px #a3a3a3";
+  var closeHeight = "translateY(-300px)";
+  var openHeight = "translateY(0px)";
+  var transSecond = "2s";
+
   // 被選到的元素可執行的函式
   Module.prototype.defaultSetting = function() {
     var btnOpts = this.options.button;
     var btn = this.$ele.find(`.${btnOpts.class}`);
+    var classStates = this.options.class;
 
     if (this.options.openAtStart) {
       btn.text(btnOpts.closeText);
     } else {
-      this.$ele.addClass("close");
+      this.$ele.addClass(classStates.closed);
       changeHeight.call(this, closeHeight);
       btn.text(btnOpts.openText);
     }
@@ -68,7 +69,6 @@
         typeof methods == "object" && methods,
         typeof options == "object" && options
       );
-      console.log(opts);
       var module = new Module(el, opts);
       // 將每個元素帶入函式
       module.defaultSetting();
@@ -81,20 +81,47 @@
   function bannerAnima() {
     var btnOpts = this.options.button;
     var btn = this.$ele.find(`.${btnOpts.class}`);
+    var isTransition = this.options.transition;
+    var classStates = this.options.class;
+    var allClassesArray = [];
+    for (var prop in classStates) allClassesArray.push(prop);
+    var allClasses = allClassesArray.join(" ");
+    console.log(allClasses);
 
-    if (this.options.transition) this.$ele.css("transition", transSecond);
+    if (isTransition) this.$ele.css("transition", transSecond);
 
-    this.$ele.toggleClass("close");
+    this.$ele.toggleClass(classStates.closed);
     if (btn.text() == btnOpts.closeText) {
       btn.text(btnOpts.openText);
-      if (this.options.transition) {
+      if (isTransition) {
+        this.$ele.removeClass(allClasses);
+        this.$ele.addClass("closing");
         setTimeout(changeHeight.bind(this, closeHeight), 2000);
+        setTimeout(
+          function() {
+            this.$ele.removeClass(allClasses);
+            this.$ele.addClass("closed");
+          }.bind(this),
+          2000
+        );
       } else {
         changeHeight.call(this, closeHeight);
       }
     } else {
       btn.text(btnOpts.closeText);
       changeHeight.call(this, openHeight);
+      if (isTransition) {
+        this.$ele.removeClass(allClasses);
+        this.$ele.addClass("opening");
+        setTimeout(
+          function() {
+            this.$ele.removeClass(allClasses);
+            this.$ele.addClass("opened");
+          }.bind(this),
+          2000
+        );
+      } else {
+      }
     }
   }
 
@@ -109,11 +136,11 @@ $(function() {
       openAtStart: false,
       autoToggle: false,
       button: {
-        closeText: "OPEN!",
-        openText: "CLOSE",
+        closeText: "CLOSE",
+        openText: "OPEN!",
         class: "btn"
       },
-      transition: false
+      transition: true
     })
     .addClass("test_class");
 });
