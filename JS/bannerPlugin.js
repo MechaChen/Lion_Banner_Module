19519bc84bbd0fa98d37e3252088d6628922786b
Benson <tumlivein@gmail.com>
2019-08-26 11:22:03 +0800

    5. 建立模組的 Constructor

--- JS/bannerPlugin.js
+++ JS/bannerPlugin.js
@@ -1,7 +1,15 @@
 (function($) {
+  var ModuleName = "banner";
+
+  var Module = function(ele, options) {
+    this.ele = ele;
+    this.$ele = $(ele);
+    this.options = options;
+  };
+
   var shadow = "3px 3px 3px #a3a3a3";
 
-  $.fn.banner = function() {
+  $.fn[ModuleName] = function(methods, options) {
     return this.each(function(index, el) {
       $(el).css("boxShadow", shadow);
     });
